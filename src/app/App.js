9d8ca2d2618da1c9fa74944d644d193f2a7e00fa
yurong <rong.yu@thoughtworks.com>
2020-08-13 23:36:46 +0800

    [yurong] feat: complete home

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
+import {Route, BrowserRouter, NavLink,Switch} from "react-router-dom";
 import Home from "./Home";
+import Calculate from "../component/calculate/Calculate";
+import Timer from "../component/clock/Timer"
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <BrowserRouter>
-          <Route exact path='/' component={Home}/>
+          <nav className="header">
+            <ul>
+              <li><NavLink className="link" exact to="/timer" >在线倒计时器</NavLink></li>
+              <li><NavLink className="link" exact to="/calculate">在线计算器</NavLink></li>
+              <li><NavLink className="link" exact to="/">HOME</NavLink></li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/calculate" component={Calculate}></Route>
+            <Route exact path="/timer" component={Timer}></Route>
+          </Switch>
         </BrowserRouter>
       </div>
     );
--- src/app/Home.js
+++ src/app/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './home.less';
+import Hero from '../images/hero-image.png'
+import claPic from '../images/calculator.png'
+import timerPic from '../images/timer.png'
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
   return (<div className="home">
-    This is home page
+    <div><img className="heroPic" src={Hero}></img></div>
+    <div className="icon">
+      <div>
+      <NavLink to='/calculate'><img className="claPic" src={claPic}></img></NavLink>
+      </div>
+      <div>
+        <NavLink to='/timer'><img className="timerPic" src={timerPic}></img></NavLink>
+      </div>
+    </div>
   </div>);
 };
 
